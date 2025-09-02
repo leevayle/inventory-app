11c6f24b5bb2e7531bed5dd6aaf5b0b0ae5e4b46
lee <leevayle@protonmail.com>
2025-09-02 09:32:28 +0300

    welcome page with layout

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,12 +4,14 @@ function Welcome(){
 
         <>
 
-            <div className="flex w-[50vw] min-w-[450px] h-[70vh] m-auto shadow-lg rounded-xl">
-                <div className="w-1/3 bg-gray-300 rounded-tl-xl rounded-bl-xl bg-[url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] bg-cover bg-center">
-                    <button className="m-4 p-2 bg-white rounded shadow hover:bg-gray-100">Home</button>
+            <div className="flex flex-col sm:flex-row w-[50vw] min-w-[300px] h-[70vh] m-auto shadow-lg rounded-xl border border-[var(--primary)] sm:min-w-[550px] ">
+                <div className="sm:w-1/3 w-full bg-white rounded-tl-xl rounded-tr-xl sm:rounded-tr-none sm:rounded-bl-xl bg-[url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] bg-cover bg-center">
+                    <button className="m-4 p-2 bg-[var(--accent)] rounded shadow hover:bg-gray-100">Home</button>
                 </div>
-                <div className="flex-1 bg-blue-100 rounded-tr-xl rounded-br-xl">
-
+                <div className="flex-1 bg-gray-100  rounded-bl-xl rounded-br-xl sm:rounded-bl-none sm:rounded-tr-xl flex flex-col">
+                    <p className="text-2xl font-bold text-center mt-10 text-[var(--primary)]">Welcome</p>
+                    
+                    <button className="w-1/2 h-10 hover:w-2/3 bg-[var(--primary)] m-auto mt-20 rounded-3xl hover:bg-[var(--accent)] transition-all">Login</button>
                 </div>
             </div>
         
