6a2a56daa4f5288a292d90e970b444f1aef6589c
lee <leevayle@protonmail.com>
2026-01-24 15:57:19 +0300

    dash dev v1.0

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,11 +2,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     showSkeleton();
 
+    setTimeout(() => {
+        hideSkeleton();
+    }, 1000);
+
      const skeleton = document.getElementById('skeleton-preview');
     
     if (skeleton) {
         skeleton.addEventListener('click', function() {
-            notify('warning', 'Please wait', 'Waiting for server response, kindly wait. This might take a while', 5000);
+            notify('warning', 'Please wait', 'Waiting for server response, kindly wait. This might take a while', 500);
         });
     } else {
         console.error('Skeleton element not found!');
